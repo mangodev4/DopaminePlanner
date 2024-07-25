1126e849a669a4f1e4c2eae1da77181134e38474
Mango <mangodev4@gmail.com>
2024-07-25 15:58:53 +0900

    Initial Commit

---
+++ DopamineApp/DopamineAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DopamineAppApp.swift
+//  DopamineApp
+//
+//  Created by Yujin Son on 7/25/24.
+//
+
+import SwiftUI
+
+@main
+struct DopamineAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
