ea186d9768363cbf81a31173d4fdbed67c65f11d
Mango <mangodev4@gmail.com>
2024-07-26 15:57:45 +0900

    feat. 메인뷰 텍스트 배치

---
+++ DopamineApp/View/CalenderView.swift
@@ -0,0 +1,18 @@
+//
+//  CalenderView.swift
+//  DopamineApp
+//
+//  Created by Yujin Son on 7/26/24.
+//
+
+import SwiftUI
+
+struct CalenderView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CalenderView()
+}
