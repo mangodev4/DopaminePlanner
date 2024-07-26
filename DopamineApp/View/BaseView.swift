898da30806435755a4ff0c9475f0d2ba009b7e48
Mango <mangodev4@gmail.com>
2024-07-26 15:58:34 +0900

    feat. 메인뷰 이미지 삽입

--- DopamineApp/View/BaseView.swift
+++ DopamineApp/View/BaseView.swift
@@ -10,10 +10,29 @@ import SwiftUI
 struct BaseView: View {
     var body: some View {
         
-        //  TabView ..
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        //  MainView
+        VStack{
+            Text("00.00.00")
+                .font(.title)
+            //      dateformatter
+            HStack{
+                Text("무계획 여행")
+                Text("누적 00건")
+            }
+            HStack{
+                Text("수정된 일정")
+                Text("누적 00건")
+            }
+            .padding(.bottom, 50)
+            
+            Image(systemName: "airplane.departure")
+                .font(.title)
+            
+            }
+            //
+            
+        }
     }
-}
 
 #Preview {
     BaseView()
