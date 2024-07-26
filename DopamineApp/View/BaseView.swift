c4af7a0625c08cc8dbdebed3bf00c23708d033d3
Mango <mangodev4@gmail.com>
2024-07-26 12:40:13 +0900

    feat. 메인뷰 구현
    루트 뷰 생성

---
+++ DopamineApp/View/BaseView.swift
@@ -0,0 +1,18 @@
+//
+//  BaseView.swift
+//  DopamineApp
+//
+//  Created by Yujin Son on 7/25/24.
+//
+
+import SwiftUI
+
+struct BaseView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BaseView()
+}
