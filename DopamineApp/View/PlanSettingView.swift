48f6b79ac177fd9204c65095bb9977fb5c8e73cf
Mango <mangodev4@gmail.com>
2024-07-29 10:45:56 +0900

    feat. 캘린더 뷰 구현

---
+++ DopamineApp/View/PlanSettingView.swift
@@ -0,0 +1,18 @@
+//
+//  PlanSettingView.swift
+//  DopamineApp
+//
+//  Created by Yujin Son on 7/27/24.
+//
+
+import SwiftUI
+
+struct PlanSettingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PlanSettingView()
+}
---
+++ DopamineApp/View/ScheduleSettingView.swift
@@ -0,0 +1,18 @@
+//
+//  ScheduleSettingView.swift
+//  DopamineApp
+//
+//  Created by Yujin Son on 7/26/24.
+//
+
+import SwiftUI
+
+struct ScheduleSettingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ScheduleSettingView()
+}
