12c21e7e738251b3296e40091440fc8a912721e7
Mango <mangodev4@gmail.com>
2024-07-30 16:43:55 +0900

    feat. 캘린더 range 디자인 변경 구현

--- DopamineApp/View/ScheduleSettingView/ScheduleSettingView.swift
+++ DopamineApp/View/ScheduleSettingView/ScheduleSettingView.swift
@@ -8,7 +8,6 @@
 import SwiftUI
 
 struct ScheduleSettingView: View {
-    @State private var isStartDate = true
     @State private var startDate: Date? = nil
     @State private var endDate: Date? = nil
    
@@ -16,30 +15,25 @@ struct ScheduleSettingView: View {
         NavigationStack {
             VStack {
                 Text(startDate == nil ? "여행 출발하는 날이 언제인가요?" : "여행 마지막 날이 언제인가요?")
-                    .font(.pretendardBold18)
+                    .font(.pretendardMedium24)
                     .padding(20)
                 
                 
                 // 캘린더 뷰 띄울 위치
                 CalenderView(month: Date(), startDate: $startDate, endDate: $endDate)
-                
-                Button(action: {
-                    isStartDate.toggle()
-                }) {
-                    Text("여행날짜 클릭")
-                        .font(.system(size: 18, weight: .bold, design: .default))
+                    .padding(.top, 20)
+                    .padding(.bottom, 50)
+
+                NavigationLink(destination: PlanSettingView()) {
+                    Text("다음")
+                        .frame(width: 200, height: 20)
+                        .font(.pretendardBold18)
                         .padding()
-                        .foregroundColor(.blue)
+                        .background(startDate != nil && endDate != nil ? Color.blue1 : Color.gray)
+                        .foregroundColor(.white)
                         .cornerRadius(10)
                 }
-                NavigationLink(destination: PlanSettingView()) {
-                                   Text("다음")
-                                       .font(.system(size: 18, weight: .bold, design: .default))
-                                       .padding()
-                                       .background(isStartDate ? Color.blue1 : Color.gray)
-                                       .foregroundColor(.white)
-                                       .cornerRadius(10)
-                               }
+                .disabled(startDate == nil || endDate == nil)
             }
             .padding()
         }
@@ -51,7 +45,7 @@ struct ScheduleSettingView: View {
             // Add any additional logic you need here
         }
     }
-
+    
 }
 
 
