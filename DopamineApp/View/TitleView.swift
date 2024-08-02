13d5d4a53dcbd0cfdc9664f77f7815276b45cc65
Mango <mangodev4@gmail.com>
2024-08-03 00:26:50 +0900

    feat. 네비게이션 매니저 구현

---
+++ DopamineApp/View/TitleView.swift
@@ -0,0 +1,18 @@
+//
+//  TitleView.swift
+//  DopamineApp
+//
+//  Created by Yujin Son on 8/3/24.
+//
+
+import SwiftUI
+
+struct TitleView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TitleView()
+}
