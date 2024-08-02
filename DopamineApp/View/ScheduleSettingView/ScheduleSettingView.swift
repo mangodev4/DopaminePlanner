17f0490f339c38d6ad991fbf74b1099a60159e46
Mango <mangodev4@gmail.com>
2024-08-02 10:54:48 +0900

    feat. 캘린더뷰 헤더버튼 구현
    back버튼/여행종료 버튼 추가

--- DopamineApp/View/ScheduleSettingView/ScheduleSettingView.swift
+++ DopamineApp/View/ScheduleSettingView/ScheduleSettingView.swift
@@ -11,34 +11,44 @@ struct ScheduleSettingView: View {
     @State private var startDate: Date? = nil
     @State private var endDate: Date? = nil
     @State private var isNavigating = false
+    @State private var isNavigatingToBase = false
+    @State private var isNavigatingToPlan = false
+
    
     var body: some View {
         NavigationStack {
             VStack {
+                HeaderButtons
+                
+                Spacer()
+                
                 Text(startDate == nil ? "여행 출발하는 날이 언제인가요?" : "여행 마지막 날이 언제인가요?")
                     .font(.pretendardBold24)
-                    .padding(.bottom, 50)
+//                    .padding(.vertical, 30)
                 
+                Spacer()
+
                 
-                // 캘린더 뷰 띄울 위치
+                // 캘린더 뷰
                 CalenderView(month: Date(), startDate: $startDate, endDate: $endDate)
                     .padding(.top, 20)
                     .padding(.horizontal, 10)
                     .padding(.bottom, 100)
                     
-
+                Spacer()
+                
                 Button(action: {
                                 if startDate != nil && endDate != nil {
                                     isNavigating = true
                                 }
                             }) {
                                 Text("다음")
-                                    .frame(width: 100)
+                                    .frame(width: 300)
                                     .font(.pretendardBold18)
                                     .padding()
                                     .background(startDate != nil && endDate != nil ? Color.blue1 : Color.gray)
                                     .foregroundColor(.white)
-                                    .cornerRadius(10)
+                                    .cornerRadius(14)
                 }
                 .disabled(startDate == nil || endDate == nil)
                 
@@ -47,6 +57,7 @@ struct ScheduleSettingView: View {
                                isActive: $isNavigating,
                                label: { EmptyView() }
                            )
+                Spacer()
             }
         }
     }
@@ -58,7 +69,39 @@ struct ScheduleSettingView: View {
         }
     }
     
+    // MARK: 헤더 버튼 뷰
+    private var HeaderButtons: some View {
+        HStack {
+            Button(action: {
+                if let window = UIApplication.shared.windows.first {
+                    window.rootViewController?.dismiss(animated: true, completion: nil)
+                }
+            }) {
+                Text("<뒤로")
+                    .font(.pretendardBold18)
+                    .foregroundColor(.gray)
+                    .underline()
+            }
+            .padding(.leading, 10)
+            
+            Spacer()
+            
+            Button(action: {
+                isNavigatingToBase = true
+            }) {
+                Text("여행 종료")
+                    .font(.pretendardBold18)
+                    .foregroundColor(.gray)
+                    .underline()
+            }
+            .padding(.trailing, 10)
+        }
+        .padding()
+    }
+    
 }
+
+
 
 
 #Preview {
